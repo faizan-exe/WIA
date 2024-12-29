a7b4f9b542d24292fa0a1c480d294a52ef26425d
RyanSikandar <ryansikandar@gmail.com>
2024-12-29 15:51:03 +0500

    feat: edit and delete for orgs

--- src/Repository/productRepo.js
+++ src/Repository/productRepo.js
@@ -35,3 +35,42 @@ export const getAllProducts = async () => {
     });
     return response.data;
 }
+
+export const deleteProduct = async (id) => {
+    console.log('Deleting product:', id);
+    const token = localStorage.getItem('token');
+    console.log('Token:', token);
+    try {
+        const response = await axios.delete(`${API_URL}/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    }
+    catch (error) {
+        console.error('Error deleting product:', error);
+        throw error;
+    }
+}
+
+export const editProduct = async (product) => {
+    const token = localStorage.getItem('token');
+    try {
+        const response = await axios.put(`${API_URL}/${product._id}`, product,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                }
+            }
+        );
+        return response.data;
+
+    }
+    catch (error) {
+        console.error('Error editing product:', error);
+        throw error;
+    }
+}
+
