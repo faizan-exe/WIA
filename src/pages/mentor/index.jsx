2af778f23e2eaa8cf2fc4bc830f975bd16c72d84
waqas2714 <waqasali00123@gmail.com>
2024-12-29 20:39:22 +0500

    minor change in mentor

--- src/pages/mentor/index.jsx
+++ src/pages/mentor/index.jsx
@@ -25,9 +25,9 @@ function Mentors() {
       <Header userRole={'woman'} />
       <div className="min-h-screen bg-gray-100 flex flex-col items-center py-8 space-y-4">
         <h1 className="text-3xl font-bold text-gray-800 mb-6">Mentor Advertisements</h1>
-        {sampleAdvertisements.ads.length === 0 && <p>No Mentor Ads Available!</p> }
+        {sampleAdvertisements.length === 0 && <p>No Mentor Ads Available!</p> }
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {sampleAdvertisements.ads.length > 0 && sampleAdvertisements.ads.map((advertisement, index) => (
+          {sampleAdvertisements.length > 0 && sampleAdvertisements.map((advertisement, index) => (
             <div key={index} className="flex flex-col items-center">
               <AdvertisementCard advertisement={advertisement} />
               
